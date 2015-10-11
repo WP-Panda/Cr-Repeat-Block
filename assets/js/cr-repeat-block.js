4c435d82692df415a3f7b3802e3a600a6dab63c4
Максим <yoowordpress@yandex.ru>
2015-10-12 02:36:45 +0300

    Добавлен перевод и автоматическое копирование

--- assets/js/cr-repeat-block.js
+++ assets/js/cr-repeat-block.js
@@ -15,83 +15,28 @@ function copyTextToClipboard(text) {
     textArea.select();
 
     try {
-        var successful = document.execCommand('copy');
-        var msg = successful ? 'successful' : 'unsuccessful';
+       document.execCommand('copy');
     } catch (err) {}
 
     document.body.removeChild(textArea);
 }
 
 (function($){
+
     $(document).on('click','.copy-icon',function(e) {
         $text = $(this).prev('.copy-code').html();
         copyTextToClipboard($text);
+        $('.cr-repeat-copy-text').html(cr_repeat_object['copy_good']);
     });
+
+    $(document).on({
+        mouseenter: function () {
+            $(this).after('<span class="cr-repeat-copy-text">' + cr_repeat_object['copy_text'] + '</span>');
+            $('.cr-repeat-copy-text').css('top',$(this).offset().top - 80);
+        },
+        mouseleave: function () {
+            $('.cr-repeat-copy-text').remove();
+        }
+    }, ".copy-icon");
+
 })(jQuery);
-
-
-(function() {
-    tinymce.PluginManager.add('true_mce_button', function( editor, url ) { // id кнопки true_mce_button должен быть везде один и тот же
-        editor.addButton( 'true_mce_button', { // id кнопки true_mce_button
-            icon: 'perec', // мой собственный CSS класс, благодар€ которому € задам иконку кнопки
-            type: 'menubutton',
-            title: '¬ставить элемент', // всплывающа€ подсказка при наведении
-            menu: [ // тут начинаетс€ первый выпадающий список
-                {
-                    text: 'Ёлементы форм',
-                    menu: [ // тут начинаетс€ второй выпадающий список внутри первого
-                        {
-                            text: '“екстовое поле',
-                            onclick: function() {
-                                editor.windowManager.open( {
-                                    title: '«адайте параметры пол€',
-                                    body: [
-                                        {
-                                            type: 'textbox', // тип textbox = текстовое поле
-                                            name: 'textboxName', // ID, будет использоватьс€ ниже
-                                            label: 'ID и name текстового пол€', // лейбл
-                                            value: 'comment' // значение по умолчанию
-                                        },
-                                        {
-                                            type: 'textbox', // тип textbox = текстовое поле
-                                            name: 'multilineName',
-                                            label: '«начение текстового пол€ по умолчанию',
-                                            value: 'ѕривет',
-                                            multiline: true, // большое текстовое поле - textarea
-                                            minWidth: 300, // минимальна€ ширина в пиксел€х
-                                            minHeight: 100 // минимальна€ высота в пиксел€х
-                                        },
-                                        {
-                                            type: 'listbox', // тип listbox = выпадающий список select
-                                            name: 'listboxName',
-                                            label: '«аполнение',
-                                            'values': [ // значени€ выпадающего списка
-                                                {text: 'ќб€зательное', value: '1'}, // лейбл, значение
-                                                {text: 'Ќеоб€зательное', value: '2'}
-                                            ]
-                                        }
-                                    ],
-                                    onsubmit: function( e ) { // это будет происходить после заполнени€ полей и нажатии кнопки отправки
-                                        editor.insertContent( '[textarea id="' + e.data.textboxName + '" value="' + e.data.multilineName + '" required="' + e.data.listboxName + '"]');
-                                    }
-                                });
-                            }
-                        },
-                        { // второй элемент вложенного выпадающего списка, прост вставл€ет шорткод [button]
-                            text: ' нопка отправки',
-                            onclick: function() {
-                                editor.insertContent('[button]');
-                            }
-                        }
-                    ]
-                },
-                { // второй элемент первого выпадающего списка, просто вставл€ет [misha]
-                    text: 'Ўорткод [misha]',
-                    onclick: function() {
-                        editor.insertContent('[misha]');
-                    }
-                }
-            ]
-        });
-    });
-})();
--- cr-repeat-block.php
+++ cr-repeat-block.php
@@ -2,8 +2,8 @@
 /*
  Plugin Name: CR Repeat Block
  Plugin URI: https://github.com/WP-Panda/Cr-Repeat-Block
- Description: Плагин предназначен для добавления повторяющихся блоков контента в разные записи.
- Version: 1.5.0
+ Description: The plugin is designed to add blocks of content repeated in different recordings.
+ Version: 1.5.1
  Author: WP_Panda
  Author URI: http://wp-panda.com/pay
  Text Domain: cr-repeat-block
@@ -30,49 +30,62 @@
 if( !defined( 'ABSPATH' ) ) exit;
 
 
-register_activation_hook( __FILE__, 'myplugin_activate' );
+register_activation_hook( __FILE__, 'cr_repeat_block_activate' );
+
+add_action( 'init', 'cr_repeat_block_flush_rewrite_rules_maybe', 20 );
+add_action('plugins_loaded', 'cr_repeat_block_init');
+add_action('admin_enqueue_scripts', 'cr_repeat_block_register_scripts');
+add_action( 'init', 'cr_repeat_block_create_cr_contents_blocks' );
+add_filter('manage_cr-contents-blocks_posts_columns', 'cr_repeat_block_cr_columns_register', 5);
+add_action( 'manage_cr-contents-blocks_posts_custom_column', 'cr_repeat_block_columns_display', 10, 2 );
+add_shortcode("cr_repeat_block", "cr_repeat_block_short");
+add_action('admin_head', 'button_js');
+add_action('media_buttons','add_sc_select',11);
+
+
 
 /**
  * Добавление флага для сброса правил перезаписи.
  */
-function myplugin_activate() {
-    if ( ! get_option( 'cr_rep_block_flush_rewrite_rules_flag' ) ) {
-        add_option( 'cr_rep_block_flush_rewrite_rules_flag', true );
+function cr_repeat_block_activate() {
+    if ( ! get_option( 'cr_repeat_block_flush_rewrite_rules_flag' ) ) {
+        add_option( 'cr_repeat_block_flush_rewrite_rules_flag', true );
     }
 }
 
-
-
-add_action( 'init', 'cr_rep_block_flush_rewrite_rules_maybe', 20 );
 /**
  * Flush rewrite rules if the previously added flag exists,
  * and then remove the flag.
  */
-function cr_rep_block_flush_rewrite_rules_maybe() {
-    if ( get_option( 'cr_rep_block_flush_rewrite_rules_flag' ) ) {
+function cr_repeat_block_flush_rewrite_rules_maybe() {
+    if ( get_option( 'cr_repeat_block_flush_rewrite_rules_flag' ) ) {
         flush_rewrite_rules();
-        delete_option( 'cr_rep_block_flush_rewrite_rules_flag' );
+        delete_option( 'cr_repeat_block_flush_rewrite_rules_flag' );
     }
 }
 
-
-add_action('plugins_loaded', 'cr_rep_block_init');
-function cr_rep_block_init(){
+function cr_repeat_block_init(){
     load_plugin_textdomain( 'cr-repeat-block', false, dirname( plugin_basename( __FILE__ ) ). '/languages/'  );
 }
 
 /**
  * Подключает стили и скрипты
  */
-add_action('admin_enqueue_scripts', 'cr_rep_block_register_scripts');
-function cr_rep_block_register_scripts($hook) {
+function cr_repeat_block_register_scripts($hook) {
 
-    wp_register_script('custom_jquery', plugins_url('/assets/js/cr-repeat-block.js', __FILE__), array('jquery'), '2.5.1');
+    wp_register_script('cr_repeat_block_js', plugins_url('/assets/js/cr-repeat-block.js', __FILE__), array('jquery'), '2.5.1');
+    $translation_array = array(
+        'copy_text' => __( 'Copy to clipboard','cr-repeat-block' ),
+        'copy_good' => __( 'Copied','cr-repeat-block' )
+    );
+    wp_localize_script( 'cr_repeat_block_js', 'cr_repeat_object', $translation_array );
+
+
     wp_register_style('new_style', plugins_url('/assets/css/style.css', __FILE__), false, '1.0.0', 'all');
 
     //скрипты и стили только для списка блоков
     if( 'edit.php' === $hook && isset($_GET['post_type']) && 'cr-contents-blocks' ===  $_GET['post_type']  ) {
-        wp_enqueue_script('custom_jquery');
+        wp_enqueue_script('cr_repeat_block_js');
         wp_enqueue_style('new_style');
     }
 }
@@ -80,8 +93,7 @@ function cr_rep_block_register_scripts($hook) {
 /**
  * Добавляет тип записей cr-contents-blocks
  */
-
-function cr_rep_block_create_cr_contents_blocks() {
+function cr_repeat_block_create_cr_contents_blocks() {
     register_post_type( 'cr-contents-blocks',
         array(
             'labels' => array(
@@ -95,13 +107,10 @@ function cr_rep_block_create_cr_contents_blocks() {
     );
 }
 
-add_action( 'init', 'cr_rep_block_create_cr_contents_blocks' );
-
 /**
  * Добавляет колонки для шорткодов
  */
-
-function cr_rep_block_cr_columns_register( $columns ) {
+function cr_repeat_block_cr_columns_register( $columns ) {
     $out = array();
     $i=0;
     foreach($columns as $col=>$name){
@@ -114,14 +123,10 @@ function cr_rep_block_cr_columns_register( $columns ) {
     return $out;
 }
 
-add_filter('manage_cr-contents-blocks_posts_columns', 'cr_rep_block_cr_columns_register', 5);
-
-
 /**
  * Наполняет колонку кодов и контента
  */
-
-function cr_rep_block_columns_display( $column, $id )
+function cr_repeat_block_columns_display( $column, $id )
 {
     $out = '';
     if ($column === 'post_page_id') {
@@ -131,21 +136,16 @@ function cr_rep_block_columns_display( $column, $id )
     if ($column === 'text') {
         $text = get_the_excerpt();
         if (!$text)
-            $text .= '<em style="color:#d54e21"><i class="dashicons dashicons-warning"></i>  ' . __('Content Missing', 'cr-repeat-block') . '</em>';
+            $text .= sprintf('<em style="color:#d54e21"><i class="dashicons dashicons-warning"></i>%s</em>',__('Content Missing', 'cr-repeat-block'));
         $out .= $text;
     }
-
     echo $out;
 }
-
-add_action( 'manage_cr-contents-blocks_posts_custom_column', 'cr_rep_block_columns_display', 10, 2 );
-
 
 /**
  * Добавляет шорткоды повторяющихся блоков
  */
-
-function cr_rep_block_short( $atts, $content = null ) {
+function cr_repeat_block_short( $atts, $content = null ) {
     extract( shortcode_atts( array(
         "id" => ''
     ), $atts ) );
@@ -153,12 +153,9 @@ function cr_rep_block_short( $atts, $content = null ) {
     return '<span class="short">' . $post_get->post_content . '</span>';
 }
 
-add_shortcode("cr_repeat_block", "cr_rep_block_short");
-
 /**
  * Добавляет селект с шорткодами
  */
-add_action('media_buttons','add_sc_select',11);
 function add_sc_select(){
     $out = sprintf('&nbsp;<select id="sc_select"><option>%s</option>',__('Cr Repeat Block'));
     $cr_blocks_ids = get_posts('post_type=cr-contents-blocks');
@@ -169,14 +166,11 @@ function add_sc_select(){
     $out .= '</select>';
     echo $out;
 }
-add_action('admin_head', 'button_js');
-
 
 /**
  * Скрипт кнопки визуального редактора
  */
-function button_js()
-{
+function button_js() {
     echo '<script type="text/javascript">
     jQuery(document).ready(function(){
        jQuery("#sc_select").change(function() {
